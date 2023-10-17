a3795cf334707e48c096ee1798fe2e7335cee708
aga-kaiser <agakaiser.pl@gmail.com>
2023-10-17 15:15:15 -0400

    new v

--- src/group_lessons/Class1.java
+++ src/group_lessons/Class1.java
@@ -2,6 +2,6 @@ package group_lessons;
 
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("working on project");
+        System.out.println("working on project  ");
     }
 }
